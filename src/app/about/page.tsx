bc82098c249fd4485bbd99f1b202729299ced6d4
lloydrichards <lloyd.d.richards@gmail.com>
2023-03-14 12:30:27 +0100

    feat: :sparkles: added toggle to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,12 +5,25 @@ import { atomWithHash } from "jotai-location";
 import Link from "next/link";
 
 const topicAtom = atomWithHashSync<string>("topic", "");
+const toggleAtom = atomWithHash<boolean>("toggle", false);
 
 export default function Home() {
   const topic = useAtomValue(topicAtom);
+  const [toggle, setToggle] = useAtom(toggleAtom);
   return (
     <main className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold">Lets talk about [ {topic} ]</h1>
+      <div className="form-control">
+        <label className="label cursor-pointer">
+          <span className="label-text">Remember me</span>
+          <input
+            type="checkbox"
+            className="toggle-info toggle"
+            checked={toggle}
+            onChange={() => setToggle(!toggle)}
+          />
+        </label>
+      </div>
     </main>
   );
 }
